f32101d1462f32f1e4d7a8bb5a76997fff1c4124
Lakmal Niranga <lakmalniranga@gmail.com>
2017-01-10 20:56:04 +0530

    initialize commit

---
+++ www/js/app.js
@@ -0,0 +1,56 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider){
+  $ionicConfigProvider.backButton.text('').icon('ion-chevron-left').previousTitleText(false);
+
+  $stateProvider.state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html"
+  });
+
+  $stateProvider.state('app.home', {
+    url: '/home',
+    views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html",
+          controller: 'HomeCtrl'
+        }
+      }
+  });
+
+  $stateProvider.state('app.another', {
+    url: '/another',
+     views: {
+        'menuContent' :{
+          templateUrl : 'templates/another.html',
+          controller: 'AnotherCtrl'
+        }
+      }
+  });
+
+  $urlRouterProvider.otherwise('/app/home');
+})
---
+++ www/js/controller.js
@@ -0,0 +1,7 @@
+app.controller('HomeCtrl', ['$scope', function($scope){
+
+}]);
+
+app.controller('AnotherCtrl', ['$scope', function($scope){
+
+}]);
